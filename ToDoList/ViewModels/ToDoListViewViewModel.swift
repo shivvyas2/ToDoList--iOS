59a25096e1f60fea29a5fa19de2ee8ebeb074f85
Shiv Vyas <shivvyas0209@gmail.com>
2024-06-27 16:33:12 -0400

    Issues Fixed

--- ToDoList/ViewModels/ToDoListViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListViewViewModel.swift
@@ -4,17 +4,31 @@
 //
 //  Created by Shiv Vyas on 6/24/24.
 //
-
+import FirebaseFirestore
+import FirebaseAuth
 import Foundation
 // View model for list of items
 
 class ToDoListViewViewModel: ObservableObject {
     @Published var showingNewItemView = false
-    init() {
+    private let userId: String
+    
+    init(userId: String) {
+        self.userId = userId
         
     }
     
+    /// Delete Item
+    /// - Parameter id: Item Id To Delete
+
     func delete(id: String){
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(userId)
+            .collection("todos")
+            .document(id)
+            .delete()
         
         
     }
--- ToDoList/Views/ToDoListView.swift
+++ ToDoList/Views/ToDoListView.swift
@@ -8,14 +8,14 @@ import FirebaseFirestoreSwift
 import SwiftUI
 
 struct ToDoListView: View {
-    @StateObject var viewModel = ToDoListViewViewModel()
+    @StateObject var viewModel : ToDoListViewViewModel
     @FirestoreQuery var items: [ToDoListItem]
     
 
     init(userId: String) {
         //users/<id>/todos/</entries>
         self._items = FirestoreQuery(collectionPath: "users/\(userId)/todos")
-        
+        self._viewModel = StateObject(wrappedValue: ToDoListViewViewModel(userId: userId))
     }
     
     var body: some View {
@@ -23,7 +23,7 @@ struct ToDoListView: View {
             VStack{
                 List(items) { item in
                     ToDoListItemView(item : item).swipeActions{
-                        Button("Delete") {
+                        Button("Delete"){
                             viewModel.delete(id: item.id)
                         }.tint(.red)
                     }
